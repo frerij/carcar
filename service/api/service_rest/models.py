7000646974c103273c4a6883dd7aac794b4211fb
Jordan <frerichs.jordan@gmail.com>
2022-05-10 14:12:15 -0700

    service models done

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from pkg_resources import parse_requirements
+
+from inventory.api.inventory_rest.models import VehicleModel
 
 # Create your models here.
+
+
+class AutomobileVO(models.Model):
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+
+    def __str__(self):
+        return self.vin
+
+
+class Technician(models.Model):
+    tech_name = models.CharField(max_length=200)
+    tech_num = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.tech_name
+
+
+class Appointment(models.Model):
+    owner_name = models.CharField(max_length=200)
+    vin = models.CharField(max_length=17, null=True, unique=True)
+    date = models.DateField(auto_now=False)
+    time = models.TimeField(auto_now=False)
+    reason = models.TextField()
+
+    assigned_tech = models.ForeignKey(
+        Technician,
+        related_name="technician",
+        on_delete=models.PROTECT
+    )
+    
+
